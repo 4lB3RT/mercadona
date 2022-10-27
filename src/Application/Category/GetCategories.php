d28ad9f1536925074b490dd29b173be45c99a269
Albert García Cortés <albert@MacBook-Air-de-Albert.local>
2022-10-27 22:33:38 +0200

    get categories use case

---
+++ src/Application/Category/GetCategories.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Mercadona\Application\Category;
+
+use Mercadona\Domain\Category\CategoryRepository;
+use Mercadona\Domain\Category\FinderCategoryRepository;
+
+final class GetCategories
+{
+    public function __construct(
+        private readonly FinderCategoryRepository $finderCategoryRepository,
+        private readonly CategoryRepository $categoryRepository
+    ) {}
+
+    public function execute(): void
+    {
+        
+    }
+}
