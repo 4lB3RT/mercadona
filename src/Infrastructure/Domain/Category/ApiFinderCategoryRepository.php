4df0920379e5bfc26b35bbf6077f768d5f236618
Albert García Cortés <albert@MacBook-Air-de-Albert.local>
2022-10-27 22:27:59 +0200

    implementation categories repositories infrastructure

---
+++ src/Infrastructure/Domain/Category/ApiFinderCategoryRepository.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Mercadona\Infrastructure\Domain\Category;
+
+use Mercadona\Domain\Category\FinderCategoryRepository;
+
+final class ApiFinderCategoryRepository implements FinderCategoryRepository
+{
+}
---
+++ src/Infrastructure/Domain/Category/EloquentCategoryRepository.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Mercadona\Infrastructure\Domain\Category;
+
+use Mercadona\Domain\Category\CategoryRepository;
+
+final class EloquentCategoryRepository implements CategoryRepository
+{
+}
