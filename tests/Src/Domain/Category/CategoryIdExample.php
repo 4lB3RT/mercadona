8733cab07c5ab7f0334e4fa289e63f11710a88eb
Albert García Cortés <albert@MacBook-Air-de-Albert.local>
2023-01-16 20:23:56 +0100

    create methods create and dummy in categoryIdExample

--- tests/Src/Domain/Category/CategoryIdExample.php
+++ tests/Src/Domain/Category/CategoryIdExample.php
@@ -7,6 +7,17 @@ use Tests\Mercadona\Shared\Domain\IntegerExample;
 
 final class CategoryIdExample {
 
+    public static function dummy(): CategoryId
+    {
+        return new CategoryId(1);
+    }
+
+    public static function create(
+        ?CategoryId $categoryId = null
+    ): CategoryId {
+        return $categoryId ?? self::random();
+    }
+
     public static function random(): CategoryId
     {
         return new CategoryId(IntegerExample::random());
