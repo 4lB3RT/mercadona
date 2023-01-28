47f4933a5830d8acb3079f797988165ebeecf695
Albert García Cortés <albert@MacBook-Air-de-Albert.local>
2023-01-28 17:49:39 +0100

    change product visibility

--- src/Infrastructure/Domain/Product/ApiProductRepository.php
+++ src/Infrastructure/Domain/Product/ApiProductRepository.php
@@ -12,7 +12,7 @@ final class ApiProductRepository implements ProductReadRepository
     public function findDetailProduct(ProductId $productId): Product
     {
       $client = new Client([ 'base_uri' => 'https://tienda.mercadona.es/api/']);
-      $response = $client->request('GET', 'products/'.$productId->value);
+      $response = $client->request('GET', 'products/'.$productId->value());
       
       $response = (array) json_decode($response->getBody()->getContents(), true);
             
